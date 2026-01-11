322c4c9531aba3866c7988df4f4dda344ade5acc
Roh <82229769+rohyunsang@users.noreply.github.com>
2026-01-12 00:27:27 +0900

    Test Script Add

---
+++ src/StarterGui/MainUI/LocalScript.client.lua
@@ -0,0 +1,5 @@
+-- HUDController.client.lua
+print("HUD LocalScript Loaded")
+
+-- local Players = game:GetService("Players")
+-- local player = Players.LocalPlayer
---
+++ src/StarterGui/MainUI/ScreenGui.lua
@@ -0,0 +1,5 @@
+return {
+	Name = "MainUI",
+	ClassName = "ScreenGui",
+	ResetOnSpawn = false,
+}
